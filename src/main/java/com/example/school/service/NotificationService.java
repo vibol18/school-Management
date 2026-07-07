0495b7d0fd9d0a50e717e58f612a352ec857dae8
Chhorn Vibol <your_email@gmail.com>
2026-07-07 10:32:35 +0700

    update

--- src/main/java/com/example/school/service/NotificationService.java
+++ src/main/java/com/example/school/service/NotificationService.java
@@ -8,9 +8,9 @@ import com.example.school.dto.NotificationResponse;
 public interface NotificationService {
     NotificationResponse create(NotificationRequest req);
 
-    List<NotificationResponse> getAll();
+    NotificationResponse markAsRead(Long
 
-    NotificationResponse markAsRead(Long id);
-
+    
     void clearNotification(Long id);
-}
+
+    
